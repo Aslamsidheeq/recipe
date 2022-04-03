49d6ba439d277b0ee70c1dc09ba3620be188e3ab
Aslamsidheeq <aslamsidhik22@gmail.com>
2022-04-03 16:09:16 +0530

    inst some components

--- src/Components/Popular.jsx
+++ src/Components/Popular.jsx
@@ -1,6 +1,38 @@
+import { useEffect } from "react";
+import { useState } from "react";
+
+
 function Popular() {
+
+  const[popular,setPopular]=useState([]);
+
+  //To work getPopular initially without calling ; useEffect.
+  useEffect(() => {
+    getPopular();
+  }, []);
+  
+    const getPopular = async() => {
+      // nine random popular dishes from spoonacular api
+        const api = await fetch('https://api.spoonacular.com/recipes/random?apiKey=0a0e5b7573294e9b906ad1f0957f596c&number=9')
+        const data = await api.json();
+        
+        //useState
+        setPopular(data.recipes)
+        console.log(popular)
+      }
   return (
-    <div>Popular</div>
+    <div>
+     {
+       popular.map((recipe)=>{
+        return(
+          //for unique key property
+           <li key={recipe.id}>
+            {recipe.title}
+            </li>
+        )
+       })}
+     
+    </div>
   )
 }
 
